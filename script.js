46f477b25995530e7bf94bec0edd95d38e59588d
KingTugce <kingstugce@gmail.com>
2023-05-08 22:47:33 -0700

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+let colorList = []
+
+for (let i = 1; i <=3; i++) {
+    let color = prompt('Please enter color number' + i + ':')
+    colorList.push(color)
+}
+
+alert('your list of color is: ' + colorList)
